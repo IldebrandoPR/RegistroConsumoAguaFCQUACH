ae9d4759bc1d41614e50b8e4682a7688819283d9
IldebrandoPR <112423198+IldebrandoPR@users.noreply.github.com>
2022-09-02 19:10:01 -0500

    Update 20220902ConsumoAguaApp.py

--- 20220902ConsumoAguaApp.py
+++ 20220902ConsumoAguaApp.py
@@ -9,20 +9,22 @@ st.title('Consumo de agua en FCQ')
 #
 
 # Create a connection object.
-conn = connect()
-
+DATE_COLUMN = 'Fecha'
+sheet_url = st.secrets["public_gsheets_url"]
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(sheet_url, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+data = load_data(5)
+st.subheader('Raw data')
+st.write(data)
 
 # Print results.
 #st.write(rows)
-for row in rows:
-    st.write(f"{row.Fecha} {row.Hora}")
+#for row in rows:
+#    st.write(f"{row.name} has a :{row.pet}:")
